d3af2c7074dc130394839925464b86188bdfec1d
imvictorlstmdev <imvictorlstarck@gmail.com>
2024-10-28 04:09:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Interatividade dos botões de aposta
-const betButtons = document.querySelectorAll('.btn-primary');
 
-betButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        alert('Aposta registrada com sucesso!');
-    });
-});
